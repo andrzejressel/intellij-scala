2035dff56a1f9440bace7b288d27684e67919ad3
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2024-03-29 11:28:20 +0100

    [prod/test sources sbt]: compilation issues #SCL-21157
    - add new build process parameter for prod/test sources
    - modification in shared sources dependencies

--- scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/ScalaBuildProcessParametersProvider.scala
+++ scala/compiler-integration/src/org/jetbrains/plugins/scala/compiler/ScalaBuildProcessParametersProvider.scala
@@ -8,7 +8,6 @@ import org.jetbrains.plugins.scala.compiler.data.SbtData
 import org.jetbrains.plugins.scala.project.ProjectExt
 import org.jetbrains.plugins.scala.settings.{ScalaCompileServerSettings, ScalaHighlightingMode}
 import org.jetbrains.sbt.SbtUtil
-import org.jetbrains.sbt.project.settings.SbtProjectSettings
 
 import java.util.Collections
 import scala.jdk.CollectionConverters._
@@ -23,6 +22,7 @@ class ScalaBuildProcessParametersProvider(project: Project)
           parallelCompilationOptions() ++
           addOpens() ++
           transitiveProjectDependenciesParams() ++
+          prodTestSourcesSeparatedParams() ++
           java9rtParams() :+
           scalaCompileServerSystemDir() :+
           // this is the only way to propagate registry values to the JPS process
@@ -33,6 +33,11 @@ class ScalaBuildProcessParametersProvider(project: Project)
   private def transitiveProjectDependenciesParams(): Seq[String] = {
     val projectTransitiveDependenciesUsed = SbtUtil.isBuiltWithProjectTransitiveDependencies(project)
     Seq(s"-Dsbt.process.dependencies.recursively=${!projectTransitiveDependenciesUsed}")
+  }
+
+  private def prodTestSourcesSeparatedParams(): Seq[String] = {
+    val projectProdTestSourcesSeparated = SbtUtil.isBuiltWithProdTestSourcesSeparated(project)
+    Seq(s"-Dsbt.prod.test.separated=$projectProdTestSourcesSeparated")
   }
 
   private def customScalaCompilerInterfaceDir(): Option[String] = {
--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/sources/SharedSourceDependenciesProviderService.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/sources/SharedSourceDependenciesProviderService.scala
@@ -12,8 +12,40 @@ class SharedSourceDependenciesProviderService extends SourceDependenciesProvider
 
     val dependencies = modules.flatMap(_.getDependenciesList.getDependencies.asScala)
 
-    dependencies.collect {
-      case it: JpsModuleDependency if it.getModule != null && it.getModule.getModuleType == SharedSourcesModuleType.INSTANCE => it.getModule
+    // should be in sync with org.jetbrains.sbt.project.settings.SbtProjectSettings.separateProdAndTestSources
+    val prodTestSourcesSeparated = Option(System.getProperty("sbt.prod.test.separated")).flatMap(_.toBooleanOption).getOrElse(false)
+
+    val isTargetMain = isMainModule(chunk.representativeTarget().getModule.getName)
+    val isSuitableName: JpsModule => Boolean = { jpsModule =>
+      // note: when modules are separated to main/test in projects that contain shared sources its test module will have dependencies
+      // to shared sources test module and shared sources main module. Because of this, we cannot treat shared sources main module as a source dependency for test module.
+      // To decide what shared sources module should be taken into account we are checking module names suffixes - main modules should add shared sources main modules,
+      // and the same with test.
+      val isJpsModuleMain = isMainModule(jpsModule.getName)
+      !prodTestSourcesSeparated || (isTargetMain.nonEmpty && isTargetMain == isJpsModuleMain)
     }
+
+    dependencies.collect {
+      case it: JpsModuleDependency if {
+        val module = it.getModule
+        module != null && module.getModuleType == SharedSourcesModuleType.INSTANCE && isSuitableName(module)
+      } => it.getModule
+    }
+  }
+
+  /**
+   * Returns:
+   * <ul>
+   * <li>Some(true) - if module is main module</li>
+   * <li>Some(false) - if module is test module</li>
+   * <li>None - if it is not source set module</li>
+   * </ul>
+   */
+  private def isMainModule(moduleName: String): Option[Boolean] = {
+    val mainPattern = """.*\.main(~[\d]+)?$""".r
+    val testPattern = """.*\.test(~[\d]+)?$""".r
+    if (mainPattern.matches(moduleName)) Some(true)
+    else if (testPattern.matches(moduleName)) Some(false)
+    else None
   }
 }
